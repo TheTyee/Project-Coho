3213548fa2eca28c5305342a9b8aba95fb159e47
Phillip Smith <phillip@communitybandwidth.ca>
2010-12-20 14:47:01 -0500

    Trying out tinysrc for image re-sizing

--- app/02-templates.js
+++ app/02-templates.js
@@ -13,16 +13,16 @@ var storyListTpl = new Ext.Template(
   '</tpl>'
 );
 
-var storyDetailTpl = new Ext.Template(
+var storyDetailTpl = new Ext.XTemplate(
     '<div id="{uuid}" class="storyDetail">',
     '<h1>{title}</h1>',
     '<h2>{abstract}</h2>',
     '<div class="meta"><span class="byline">By {byline}.</span> <span class="published">{storyDate}</span></div>',
-    //' <tpl for="related_media[0].thumbnails[0]">',
+    ' <tpl for="related_media[0]">',
     //'   <tpl if="width == 300">',
-    //'       <img src="{uri}" class="thumnail" />',
+    '       <img src="http://i.tinysrc.mobi/{uri}" class="thumnail" />', // Trying out tinysrc
     //'   </tpl>',
-    //' </tpl>',
+    ' </tpl>',
     '<div class="storyContent">{content}</div>',
     '</div>'
 );
