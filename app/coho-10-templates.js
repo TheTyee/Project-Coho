46372b3aeeaf1fcd7209627c3ba2ece3f9b5934b
Phillip Smith <phillip@communitybandwidth.ca>
2011-02-01 11:47:55 -0500

    Added HTML for the More tab

--- app/coho-10-templates.js
+++ app/coho-10-templates.js
@@ -96,15 +96,50 @@ slideshowSlide: new Ext.Template(
     '<p>{caption:ellipsis("140",1)}</p>'
 ),
 
-morePanel: new Ext.Template(
-    '<p>This is the MORE panel!</p>'
-),
-
 // note: savedHelpHTML and searchHelpHTML are straight text,
 // NOT Ext.Template objects!
-savedHelpHTML: 'Tap the arrow at the top right of a story to save it to this page.<br />Swipe a story to delete it from this page.',
+savedHelpHTML: 'Tap the arrow at the top right of a story to save it. Swipe a story to delete it from this page.',
 
 searchHelpHTML: 'Tap the search box and type to find any Tyee story.',
 
-};
+morePanel: new Ext.Template(
+    '<div class="storyDetail">',
+    '<h1>Application Help</h1>',
+    '<ul>',
+    '   <li><p><a href="#save">Save app to phone home screen</a></p></li>',
+    '   <li><p><a href="#about">About this app</a></p></li>',
+    '   <li><p>Go to <a href="http://thetyee.ca">the full version of TheTyee.ca</a></p></li>',
+    '   <li><p>Find out more <a href="http://thetyee.ca/About/Intro/">about The Tyee</a></p></li>',
+    '   <li><p><a href="mailto:info@thetyee.ca?subject=Mobile%20App%20Feedback">Send feedback on this app</a></p></li>',
+    '   <li><p><a href="http://subscribe.thetyee.ca/" target="_blank">Sign up for our FREE newsletter!</a></p></li>',
+    '<h2 id="save">Add this app to your phone</h2>',
+    '<ol>',
+    '<li>Open a web browser and go to <strong><a href="http://app.thetyee.ca">http://app.thetyee.ca</a></strong>.',
+    '<p><img width="150" height="255" src="img/step1.jpg"></p></li>',
+    '<li>Tap arrow at the bottom of screen.',
+    '<p><img width="150" height="255" src="img/step2.jpg"></p></li>',
+    '<li>Tap <strong>Save to Home Screen</strong>.',
+    '<p><img width="150" height="255" src="img/step3.jpg"></p></li>',
+    '<li>Tap <strong>Add</strong>.',
+    '<p><img width="150" height="255" src="img/step4.jpg"></p></li>',
+    '<li>Enjoy!',
+    '<p><img width="150" height="255" src="img/step5.jpg"></p>',
+    '</li></ol>',
+    '<h2 id="about">About The Tyee News App</h2>',
+    '<p>The Tyee news app was built by Phillip Smith and Greg Heo using ',
+    '<a href="http://http://www.sencha.com/products/touch/">Sencha Touch</a>, a HTML5 mobile JavaScript framework</p>',
+    '<p>The source code for this application is open source and can be found on ',
+    '<a href="https://github.com/phillipadsmith/A-Slippery-Fish">Github</a></p>',
+    '<p>It\'s available for most devices with a touch-screen interface. ',
+    'If you\'re using a Blackberry, you must also be using OS 6.0 or later.</p>',
+    '<p>Instructions on how to add this app to your phone are <a href="#save">provided above</a>.</p>',
+    '<p>The content of this app updates each time you open it (and are connected to a network).',
+    'To save a story for later reading, go to a story, tap the arrow in the top-right corner, and choose save.',
+    'This adds the story to the Saved screen. To remove the story, simply swipe your finger across it.</p>',
+    '<p>To search for any story on TheTyee.ca, go to the Search screen and type the search terms in the box',
+    'at the top of the screen. This searches the entire Tyee archive (and requires an Internet connection).</p>',  
+    '<p>To see The Tyee team, go <a href="http://www.youtube.com/watch?v=32UGD0fV45g" target="_blank">here</a>.</p>',
+    '</div>'
+),
 
+};
